7d3686996eb50a17253e3fd609ae27bacabb3dc4
Bernard1010 <bernahall1010@gmail.com>
2016-03-08 20:48:47 -0800

    nombre del creador del archivo

--- MUX8_1C.v
+++ MUX8_1C.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Bernardo Rodriguez Hall
 // 
 // Create Date:    23:38:10 03/03/2016 
 // Design Name: 
